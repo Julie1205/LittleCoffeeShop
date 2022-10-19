b21f24f96c26c43ba2053665907a552e043ae069
Julie Hoang <juliehoang12@hotmail.com>
2022-10-19 13:15:10 -0400

    GlobalStyles and packages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./components/GlobalStyles";
 
 const App = () => {
   return (
-    <div>
-      hi
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/Menu" element={<p>Menu</p>} />
+        <Route path="/order" element={<p>Your Order</p>} />
+        <Route path="/admin" element={<p>LogIn</p>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
