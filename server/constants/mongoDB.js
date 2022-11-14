8997d89d3d7ff9c68647389dddb1b573775f32de
Julie Hoang <juliehoang12@hotmail.com>
2022-11-14 13:38:13 -0500

    added user creation endpoint

--- server/constants/mongoDB.js
+++ server/constants/mongoDB.js
@@ -1,4 +1,5 @@
 const DATABASE_NAME = "coffeeShop";
 const MENU_COLLECTION = "menu";
+const USERS_COLLECTION = "users";
 
-module.exports = { DATABASE_NAME, MENU_COLLECTION };
+module.exports = { DATABASE_NAME, MENU_COLLECTION, USERS_COLLECTION };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const bcrypt = require("bcrypt");
 
 const { 
     connectToMongoDb,  
     closeMongoDb 
 } = require("./connection/mongoDB");
 
-const { MENU_COLLECTION } = require("./constants/mongoDB");
+const { MENU_COLLECTION, USERS_COLLECTION } = require("./constants/mongoDB");
+const { validateForm } = require("./validations");
 
 const app = express();
 const port = 8000;
@@ -21,6 +23,44 @@ app.get("/menu", async(req, res) => {
         return res.status(404).json( { status: 404, message: "Menu not found." } );
     } else {
         return res.status(200).json({status: 200, data: results});
+    }
+});
+
+app.post("/user", async(req, res) => {
+    const db = req.app.locals.db;
+    const {
+        firstName,
+        lastName,
+        email,
+        password
+    } = req.body;
+
+    if(validateForm(firstName, lastName, email, password)) {
+        //TO DO: check if email already in use
+
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+            const newUserInfo = {
+                firstName: firstName.trim(),
+                lastName: lastName.trim(),
+                email: email.toLowerCase().trim(),
+                password: hash
+            };
+
+            try {
+                const result = await db.collection(USERS_COLLECTION).insertOne( newUserInfo );
+                if(result.insertedId) {
+                    return res.status(201).json( { status: 201, message: "user created." } );
+                } else {
+                    return res.status(500).json( { status: 500, message: "unable to create user" } )
+                }
+            } catch (err) {
+                console.log(err.stack);
+                res.status(500).json( { status: 500, message: err.message } );
+            }
+        })
+    } else {
+        return res.status(404).json( {status: 404, data: req.body, message: "Values entered are invalid." } )
     }
 });
 
